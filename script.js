7313fd01c701536bfbde48f23e264a0775dad3b9
Renato-Rhuan <renato.rhuan@icloud.com>
2025-04-13 16:38:00 -0300

    primeira versao

---
+++ script.js
@@ -0,0 +1,8 @@
+// TEXTO ANIMADO
+var typed = new Typed(".multiple-text", {
+    strings: ["Web", "Criativo"],
+    typeSpeed: 100,   // Velocidade de digitação (em milissegundos)
+    backSpeed: 100,   // Velocidade de apagamento
+    backDelay: 1000,  // Atraso antes de começar a apagar o texto
+    loop: true        // Continuar o loop da animação
+});
