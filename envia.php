529f4189469236e85348b89c38d6abe86455dc7c
Renato-Rhuan <renato.rhuan@icloud.com>
2025-05-04 12:45:42 -0300

    quarta versao

---
+++ envia.php
@@ -0,0 +1,22 @@
+<?php
+
+    $nome = addcslashes($_POST['nome']);
+    $email = addcslashes($_POST['email']);
+    $telefone = addcslashes($_POST['telefone']);
+    $descricao = addcslashes($_POST['descricao']);
+
+    $para = "renatoclahs12@gmail.com";
+    $assunto = "Fale comigo - Portfolio";
+
+    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone."\n"."Descrição: ".$descricao;
+
+    $cabeca = "From: renatoclahs12@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/". phpversion();
+
+    if(mail($para,$assunto,$corpo,$cabeca)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("Houve um erro ao enviar o email!")
+    }
+
+
+?>
