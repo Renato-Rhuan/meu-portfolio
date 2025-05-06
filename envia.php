3f865a094e75b50e0c27d5674e07a2b0bdad7b17
Renato Rhuan <renato.rhuan@icloud.com>
2025-05-06 14:15:48 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -15,7 +15,7 @@
     if(mail($para,$assunto,$corpo,$cabeca)){
         echo("E-mail enviado com sucesso!");
     }else{
-        echo("Houve um erro ao enviar o email!")
+        echo("Houve um erro ao enviar o email!");
     }
 
 
